e391fe1104649e628453edde362ee9c686767feb
vzhu <victorrzhu@gmail.com>
2016-07-11 04:43:18 -0700

    First three requirements missed some files
    Users can create a new game in any Slack channel by challenging another
    user (using their username).
    A channel can have at most one game being played at a time.
    Anyone in the channel can run a command to display the current board and
    list whose turn it is.

--- TicTacToe/TicTacToe/Controllers/TTTController.cs
+++ TicTacToe/TicTacToe/Controllers/TTTController.cs
@@ -5,37 +5,85 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TicTacToe.Controllers
 {
     public class TTTController : ApiController
     {
-        List<Board> boards;
+        BoardTable model = BoardTable.Instance;
 
         public TTTController()
         {
-            boards = new List<Board>();
-            var board = new Board();
+        }
 
-            for (int i = 0; i < 3; i++)
+        [HttpPost]
+        [Route("api/ttt/create")]
+        public IHttpActionResult CreateBoard([FromBody]JObject data)
+        {
+            string channel = data["channel_id"].ToObject<string>();
+            if(!this.model.PreCreateBoardValidation(channel))
             {
-                for (int j = 0; j < 3; j++)
-                {
-                    board.Grid[i, j] = Player.One;
-                }
+                return BadRequest("Could not create board, there is already a session in place");
             }
 
-            boards.Add(board);
+            string player1 = data["user_name"].ToObject<string>();
+            string player2 = data["text"].ToObject<string>();
+
+            if (!this.model.CreateBoard(player1, player2, channel))
+            {
+                return BadRequest("Could not create board, invalid user input");
+            }
+
+            return Ok();
         }
 
-        public List<Board> GetAllBoards()
+        [HttpPost]
+        [Route("api/ttt/delete")]
+        public IHttpActionResult DeleteBoard([FromBody]JObject data)
         {
-            return boards;
+            string channel = data["channel_id"].ToObject<string>();
+            string player1 = data["user_name"].ToObject<string>();
+            string player2 = data["text"].ToObject<string>();
+
+            if (!this.model.DeleteBoard(player1, player2, channel))
+            {
+                return BadRequest("Could not delete board, invalid user input");
+            }
+
+            return Ok();
         }
 
-        public IHttpActionResult GetBoard(int id)
+        [HttpPost]
+        [Route("api/ttt/get")]
+        public IHttpActionResult GetBoard([FromBody]JObject data)
         {
-            return Ok(boards);
+            string channel = data["channel_id"].ToObject<string>();
+
+            Board result = this.model.GetBoard(channel);
+            if (result == null)
+            {
+                return BadRequest("Could not delete board, invalid user input");
+            }
+
+            return Ok(result);
         }
+
+        //[HttpPost]
+        //[Route("api/ttt/update")]
+        //public IHttpActionResult UpdateBoard([FromBody]JObject data)
+        //{
+        //    string player1 = data["user_name"].ToObject<string>();
+        //    string position = data["text"].ToObject<string>();
+
+        //    Board result = this.model.UpdateBoard(player1, position);
+        //    if (result == null)
+        //    {
+        //        return BadRequest("Could not delete board, invalid user input");
+        //    }
+
+        //    return Ok(result);
+        //}
     }
 }
---
+++ TicTacToe/TicTacToe/Models/BoardTable.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicTacToe.Models
+{
+    public class BoardTable
+    {
+        private static string delimiter = "__";
+
+        private static BoardTable instance;
+
+        public Dictionary<string, Board> userToBoardDb { get; private set; }
+        public Dictionary<string, Board> channelToBoardDb { get; private set; }
+
+        private BoardTable()
+        {
+            this.userToBoardDb = new Dictionary<string, Board>();
+            this.channelToBoardDb = new Dictionary<string, Board>();
+        }
+
+        public static BoardTable Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BoardTable();
+                }
+                return instance;
+            }
+        }
+
+        public bool PreCreateBoardValidation(string channel)
+        {
+            if (this.channelToBoardDb.Keys.Contains(channel))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CreateBoard(string player1, string player2, string channel)
+        {
+            if (string.IsNullOrEmpty(player1) || string.IsNullOrEmpty(player2) || (string.Compare(player1, player2) == 0))
+            {
+                return false;
+            }
+
+            string player1Key = CreateKey(player1, channel);
+            string player2Key = CreateKey(player2, channel);
+
+            // already contains the key
+            if (this.userToBoardDb.ContainsKey(player1Key) || this.userToBoardDb.ContainsKey(player2Key))
+                return false;
+
+            // add the key, game
+            Board board = new Board(player1, player2, channel);
+            this.userToBoardDb.Add(player1Key, board);
+            this.userToBoardDb.Add(player2Key, board);
+            this.channelToBoardDb.Add(channel, board);
+
+            return true;
+        }
+
+        public bool DeleteBoard(string player1, string player2, string channel)
+        {
+            string player1Key = CreateKey(player1, channel);
+            string player2Key = CreateKey(player2, channel);
+
+            if (this.userToBoardDb.ContainsKey(player1Key))
+            {
+                this.userToBoardDb.Remove(player1Key);
+            }
+
+            if (this.userToBoardDb.ContainsKey(player2Key))
+            {
+                this.userToBoardDb.Remove(player2Key);
+            }
+
+            if (this.channelToBoardDb.ContainsKey(channel))
+            {
+                this.channelToBoardDb.Remove(channel);
+            }
+
+            return true;
+        }
+
+        public Board GetBoard(string channel)
+        {
+            if (string.IsNullOrEmpty(channel) || !this.channelToBoardDb.ContainsKey(channel))
+            {
+                return null;
+            }
+
+            Board result;
+            this.channelToBoardDb.TryGetValue(channel, out result);
+
+            return result;
+        }
+
+        private string CreateKey(string player, string channel)
+        {
+            if (string.IsNullOrEmpty(player) || string.IsNullOrEmpty(channel))
+            {
+                return string.Empty;
+            }
+
+            return player + delimiter + channel;
+        }
+    }
+}
