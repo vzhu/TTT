f9255aa316e5a23310a897183b4433e47c37a09f
vzhu <victorrzhu@gmail.com>
2016-07-10 23:12:27 -0700

    Base checkin
    Initial Commit.

---
+++ TicTacToe/TicTacToe/Controllers/TTTController.cs
@@ -0,0 +1,41 @@
+﻿using TicTacToe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TicTacToe.Controllers
+{
+    public class TTTController : ApiController
+    {
+        List<Board> boards;
+
+        public TTTController()
+        {
+            boards = new List<Board>();
+            var board = new Board();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board.Grid[i, j] = Player.One;
+                }
+            }
+
+            boards.Add(board);
+        }
+
+        public List<Board> GetAllBoards()
+        {
+            return boards;
+        }
+
+        public IHttpActionResult GetBoard(int id)
+        {
+            return Ok(boards);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Models/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicTacToe.Models
+{
+    public enum Player
+    {
+        None = 0,
+        One,
+        Two
+    };
+
+    public class Board
+    {
+        private int MAX_SIZE = 3;
+
+        public Player[,] Grid { get; set; }
+        public Board()
+        {
+            Grid = new Player[MAX_SIZE, MAX_SIZE];
+            
+            for (int i = 0; i < MAX_SIZE; i++)
+            {
+                for (int j = 0; j < MAX_SIZE; j++)
+                {
+                    Grid[i,j] = Player.None;
+                }
+            }
+        }
+
+        public Board CreateNewBoard()
+        {
+            return new Board();
+        }
+    }
+}
